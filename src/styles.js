e4b0cdd192acf606e13f900d91b5bd66b7f39f53
quotto <rhyme.and.rhyme@gmail.com>
2020-07-09 00:21:16 +0900

    スマートフォンブラウザ対応のためcontainerの100vh→100%に修正,html,body,#rootも合わせて修正

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,9 @@
 const styles = {
-  container: { width: '100%', height: 'calc(100vh - 66px)',  margin: '0 auto', display: 'flex',  flex: 1, flexDirection: 'column' },
+  container: { width: '100%', height: 'calc(100% - 66px)',  margin: '0 auto', display: 'flex',  flex: 1, flexDirection: 'column' },
   main: {padding: '0 20px', height: '100%', overflowY: 'scroll' },
   footerButton: {  outline: 'none', width: '100px' , padding: '12px 0px',margin: '0px 12px'},
-  footer: {display: 'flex', justifyContent: 'center', width: '100%', position: 'fixed', bottom: 16, left: 0,height: '34px' }
+  footer: {display: 'flex', justifyContent: 'center', width: '100%', position: 'fixed', bottom: 16, left: 0,height: '34px' },
+  fabContainer: {display: 'flex',position: 'fixed',}
 }
 
 export default styles
